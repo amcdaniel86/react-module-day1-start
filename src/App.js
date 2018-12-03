e99a32dfa3c5c4d757489fd157d2dbd75dcec8b3
Alex McDaniel <amcdaniel186@gmail.com>
2018-12-03 08:18:29 -0500

    initupdate and react didnt work as intended

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello Ironhackers! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// THIS FILE IS JUST A REACT COMPONENT.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+  // rendering a component named App, created next, and is rendered at the DOM element with the ID root.
+);
+
+// ReactDOM.render function tells React what to render and where to render it.
